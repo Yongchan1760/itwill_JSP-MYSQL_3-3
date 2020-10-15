c1330a9e73a7e9076947b437e3efa58c3415577b
seventy_eight <41247659+Yongchan1760@users.noreply.github.com>
2020-10-15 22:02:26 +0900

    cc

--- pro06/src/sec01/ex01/MemberServlet.java
+++ pro06/src/sec01/ex01/MemberServlet.java
@@ -1,7 +1,9 @@
 package sec01.ex01;
 
 import java.io.IOException;
+
 import java.io.PrintWriter;
+import java.sql.Date;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -32,8 +34,32 @@ public class MemberServlet extends HttpServlet {
 		//각각의 MemberVO객체들을 최종적으로 ArrayList가변길이 배열에 저장후
 		//DB에서 검색한 회원정보들(MemberVO객체들)을 담고 있는 ArrayList배열을 리턴 받는다.
 		ArrayList list = dao.listMembers();
-		
-		
+		out.print("<html><body>");
+		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입</td></tr>");
+		for (int i = 0; i < list.size(); i++) {
+			//조회한 회원정보는 arraylist라는 가변길이 배열 공간에 저장되어 있으므로 
+			//arraylist 가변길이 배열에 저장된 검색한 회원정보를 하니씩 얻는다.
+			MemberVO memberVO=(MemberVO) list.get(i);
+			
+			String id = memberVO.getId(); //검색한 id 얻기 
+			String pwd = memberVO.getPwd(); //검색한 비밀번호 얻기 
+			String name =memberVO.getName();//검색한 회원이름 얻기 
+			String email = memberVO.getEmail();//검색한 회원의 이메일주소 얻기 
+			Date joinDate = memberVO.getJoinDate();//검색한 회원의 가입날짜 정보 얻기 
+			
+			//위 변수에 저장된 조회한 각컬럼값을 다시 MemberVO객체를 생성해 
+			//그객체 내부에 있는 각각의 인스턴스 변수에 저장합니다.(setter메소드 호출)
+			
+			//조회한 회원정보를 바깥 for문과 <tr> 태그를 이용해 리스트로 출력합니다.
+			
+			out.print("<tr><td>"+id+"</td><td>"+
+								pwd+"</td><td>"+
+								name+"</td><td>"+
+								email+"</td><td>"+
+								joinDate+"</td></tr>");
+		}
+		out.print("</table></body></html>");
 	}
 
 }
